52cfe347a3f9e2f0f6bc67f43744dabd861fc2da
ParkByounghyouk <chicpark7@gmail.com>
2016-02-24 18:15:27 +0900

    * Enable tableview to scroll infinitely

--- CPWheelViewDemo/CPWheelView.swift
+++ CPWheelViewDemo/CPWheelView.swift
@@ -23,31 +23,126 @@ public class CPWheelView: UIView {
     var currentOffset: CGFloat = 0.0
     var lastTouchY: CGFloat = 0.0
     var viewArray: [CPWheelViewUnit] = [CPWheelViewUnit]()
-    let max = 100
+    let max = 30
     required public init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
         let panGesture = UIPanGestureRecognizer(target: self, action: "onPan:")
         panGesture.minimumNumberOfTouches = 1
+        var transformPerspective = CATransform3DIdentity
+        transformPerspective.m34 = -1 / 500.0
+        self.layer.sublayerTransform = transformPerspective
+        
+        
+//        CATransform3D transformPerspective = CATransform3DIdentity;
+//        transformPerspective.m34 = -1.0 / 500.0;
+//        self.layer.sublayerTransform = transformPerspective;
+
         self.addGestureRecognizer(panGesture)
         for i in 0...max {
-            var frame = CGRectMake(0, 0, 50, 50)
-            frame.origin = self.pointOnCircle(Float(180), center: self.center, index: CGFloat(i))
-            let view = UILabel(frame: frame)
+            let view = UILabel(frame: CGRectMake(0, 0, 100, 50))
             view.text = "\(i)"
             view.backgroundColor = UIColor.greenColor()
             viewArray.append(CPWheelViewUnit(view: view, position: CGFloat(i)))
             view.tag = i
+            view.center = self.pointOnCircle(Float(180), center: self.center, index: CGFloat(i))
             self.addSubview(view)
         }
+    }
+    func createBezierPath() -> UIBezierPath {
+        
+        // create a new path
+        let path = UIBezierPath()
+        
+        // starting point for the path (bottom left)
+        path.moveToPoint(CGPoint(x: 2, y: 26))
+        
+        // *********************
+        // ***** Left side *****
+        // *********************
+        
+        // segment 1: line
+        path.addLineToPoint(CGPoint(x: 2, y: 15))
+        
+        // segment 2: curve
+        path.addCurveToPoint(CGPoint(x: 0, y: 12), // ending point
+            controlPoint1: CGPoint(x: 2, y: 14),
+            controlPoint2: CGPoint(x: 0, y: 14))
+        
+        // segment 3: line
+        path.addLineToPoint(CGPoint(x: 0, y: 2))
+        
+        // *********************
+        // ****** Top side *****
+        // *********************
+        
+        // segment 4: arc
+        path.addArcWithCenter(CGPoint(x: 2, y: 2), // center point of circle
+            radius: 2, // this will make it meet our path line
+            startAngle: CGFloat(M_PI), // π radians = 180 degrees = straight left
+            endAngle: CGFloat(3*M_PI_2), // 3π/2 radians = 270 degrees = straight up
+            clockwise: true) // startAngle to endAngle goes in a clockwise direction
+        
+        // segment 5: line
+        path.addLineToPoint(CGPoint(x: 8, y: 0))
+        
+        // segment 6: arc
+        path.addArcWithCenter(CGPoint(x: 8, y: 2),
+            radius: 2,
+            startAngle: CGFloat(3*M_PI_2), // straight up
+            endAngle: CGFloat(0), // 0 radians = straight right
+            clockwise: true)
+        
+        // *********************
+        // ***** Right side ****
+        // *********************
+        
+        // segment 7: line
+        path.addLineToPoint(CGPoint(x: 10, y: 12))
+        
+        // segment 8: curve
+        path.addCurveToPoint(CGPoint(x: 8, y: 15), // ending point
+            controlPoint1: CGPoint(x: 10, y: 14),
+            controlPoint2: CGPoint(x: 8, y: 14))
+        
+        // segment 9: line
+        path.addLineToPoint(CGPoint(x: 8, y: 26))
+        
+        // *********************
+        // **** Bottom side ****
+        // *********************
+        
+        // segment 10: line
+        path.closePath() // draws the final line to close the path
+        
+        return path
+    }
+    override public func drawRect(rect: CGRect) {
+        super.drawRect(rect)
+        
+        let path = createBezierPath()
+        // fill
+        let fillColor = UIColor.whiteColor()
+        fillColor.setFill()
+        
+        // stroke
+        path.lineWidth = 1.0
+        let strokeColor = UIColor.blueColor()
+        strokeColor.setStroke()
+        
+        // Move the path to a new location
+        path.applyTransform(CGAffineTransformMakeTranslation(10, 10))
+        
+        // fill and stroke the path (always do these last)
+        path.fill()
+        path.stroke()
+
     }
     
     public override func layoutSubviews() {
         super.layoutSubviews()
         for i in 0...max {
-            var frame = CGRectMake(0, 0, 50, 50)
-            frame.origin = self.pointOnCircle(Float(180), center: self.center, index: CGFloat(i))
             let item = viewArray[i]
-            item.view.frame = frame
+            item.view.center = self.pointOnCircle(Float(180), center: self.center, index: CGFloat(i))
             item.view.backgroundColor = UIColor.greenColor()
         }
     }
@@ -55,18 +150,17 @@ public class CPWheelView: UIView {
     func onPan(panGesture: UIPanGestureRecognizer) {
         switch (panGesture.state) {
         case .Began:
-            print("Began")
+            print("=====================================================================================================")
             lastTouchY = panGesture.translationInView(self).y
-
+            print(self.viewArray[0].position)
             break
         case .Changed:
             currentOffset = (lastTouchY - panGesture.translationInView(self).y) / 100
+            print(currentOffset)
             for i in 0...max {
                 let item = viewArray[i]
                 let offset = item.position + currentOffset
-                var viewFrame = item.view.frame
-                viewFrame.origin = self.pointOnCircle(Float(180), center: self.center, index: offset)
-                item.view.frame = viewFrame
+                item.view.center = self.pointOnCircle(Float(180), center: self.center, index: offset)
 //                let centerOffset = 1 - abs(offset - CGFloat(self.viewArray.count / 2)) / 100
 //                item.view.transform = CGAffineTransformScale(CGAffineTransformIdentity, centerOffset, centerOffset)
 //                if (i == 0) {
@@ -76,48 +170,43 @@ public class CPWheelView: UIView {
             break
         case .Ended:
             currentOffset = round((lastTouchY - panGesture.translationInView(self).y) / 100)
-            let storedCurrentOffset = currentOffset
-            let animationOptions: UIViewAnimationOptions = .CurveEaseOut
-            let keyframeAnimationOptions: UIViewKeyframeAnimationOptions = UIViewKeyframeAnimationOptions(rawValue: animationOptions.rawValue)
-            let allDuration: CGFloat = 0.6
+//            let animationOptions: UIViewAnimationOptions = .CurveEaseOut
+//            let keyframeAnimationOptions: UIViewKeyframeAnimationOptions = UIViewKeyframeAnimationOptions(rawValue: animationOptions.rawValue)
+            let allDuration: CGFloat = 0.9
             for i in 0...self.max {
 
                 let item = self.viewArray[i]
-                let currentViewIndex = item.position + storedCurrentOffset
+                let currentViewIndex = item.position + currentOffset
                 let midIndex = CGFloat(self.viewArray.count / 2)
-                let nextIndex = round((currentViewIndex) - panGesture.velocityInView(self).y / 1000)
+                let nextIndex = round((currentViewIndex) - panGesture.velocityInView(self).y / 500)
 
                 let destPosition = self.pointOnCircle(Float(180), center: self.center, index: nextIndex)
                 let midPosition = self.pointOnCircle(Float(180), center: self.center, index: midIndex)
                 
-                let needPassbyMid = (midIndex > currentViewIndex) != (midIndex > nextIndex)
-                
+                let needPassbyMid = (midIndex >= currentViewIndex) != (midIndex >= nextIndex) && currentViewIndex != midIndex && nextIndex != midIndex
+                if needPassbyMid {
+                    print(needPassbyMid, currentViewIndex, nextIndex)
+                }
                 let originToMidDistance = self.distanceBetweenPoints(item.view.center, midPosition)
                 let midToDestDistance = self.distanceBetweenPoints(midPosition, destPosition)
                 
-                let originToMidDuration = allDuration * originToMidDistance / (originToMidDistance + midToDestDistance)
-                UIView.animateKeyframesWithDuration(NSTimeInterval(allDuration), delay: 0, options: keyframeAnimationOptions,
-                    animations: { () -> Void in
-                        if (needPassbyMid) {
-                            
-                            [UIView .addKeyframeWithRelativeStartTime(0, relativeDuration: Double(originToMidDuration), animations: { () -> Void in
-                                var viewFrame = item.view.frame
-                                viewFrame.origin = midPosition
-                                item.view.frame = viewFrame
-                            })]
-                        }
-                        [UIView .addKeyframeWithRelativeStartTime(Double(needPassbyMid ? originToMidDuration : 0), relativeDuration: Double(needPassbyMid ? allDuration - originToMidDuration : allDuration), animations: { () -> Void in
-                            var viewFrame = item.view.frame
-                            viewFrame.origin = destPosition
-                            item.view.frame = viewFrame
-                            
-                        })]
-                        item.position = nextIndex
-                    }, completion:{ (Bool) -> Void in
-                        item.position = nextIndex
-                    }
-                
-                )
+                let originToMidDuration = allDuration * (originToMidDistance / (originToMidDistance + midToDestDistance))
+                if (needPassbyMid) {
+                    UIView.animateWithDuration(Double(originToMidDuration), delay: 0, options: .CurveLinear, animations: { () -> Void in
+                        item.view.center = midPosition
+                        }, completion: { (Bool) -> Void in
+                            UIView.animateWithDuration(Double(allDuration - originToMidDuration), delay: 0, options: .CurveLinear, animations: { () -> Void in
+                                    item.view.center = destPosition
+                                }, completion: nil)
+                        
+                    })
+                }
+                else {
+                    UIView.animateWithDuration(Double(allDuration), delay: 0, options: .CurveEaseIn, animations: { () -> Void in
+                        item.view.center = destPosition
+                        }, completion: { (Bool) -> Void in
+                    })
+                }
             }
             
             break
@@ -130,9 +219,9 @@ public class CPWheelView: UIView {
         let midIndex: NSInteger = self.viewArray.count / 2
         let distanceToMid = index - CGFloat(midIndex)
         
-        let x = self.frame.size.width / CGFloat(2.0) + pow(abs(distanceToMid), 0.5) * 100
-        let y = self.frame.size.height / CGFloat(2.0) + pow(abs(distanceToMid), 2) * ((index - CGFloat(midIndex) > 0) ? 40 : -40)
-        return CGPointMake(x, y)
+//        let x = self.frame.size.width / CGFloat(2.0) + pow(abs(distanceToMid), 0.5) * 100
+        let y = self.frame.size.height / CGFloat(2.0) + distanceToMid * 100
+        return CGPointMake(100, y)
     }
     
     func distanceBetweenPoints(point1: CGPoint, _ point2: CGPoint) -> CGFloat {
--- CPWheelViewDemo/ViewController.swift
+++ CPWheelViewDemo/ViewController.swift
@@ -8,18 +8,59 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UITableViewController {
+    var dataArray = [Int]()
+    var sampleInt = 0
     override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.tableView.rowHeight = 60
+        self.tableView.bounces = false
+        self.tableView.separatorStyle = .None
+        self.tableView.showsHorizontalScrollIndicator = false
+        self.tableView.showsVerticalScrollIndicator = false
+        var numberOfRows = Int(self.view.frame.size.height / self.tableView.rowHeight) + 20
+        numberOfRows = numberOfRows % 2 == 0 ? numberOfRows + 1 : numberOfRows
+        for i in 0..<numberOfRows {
+            self.dataArray.insert(i, atIndex: i)
+        }
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        self.tableView.selectRowAtIndexPath(NSIndexPath(forRow: dataArray.count / 2, inSection: 0), animated: false, scrollPosition: .Middle)
+    }
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10000
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        if indexPath.row == 0 {
+        }
+        if let cell = tableView.dequeueReusableCellWithIdentifier("TEST") {
+            cell.textLabel?.text = "\(self.dataArray[indexPath.row % self.dataArray.count])"
+            cell.selectionStyle = .Blue
+            return cell
+        }
+        return UITableViewCell()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    override func scrollViewDidScroll(scrollView: UIScrollView) {
+        let visibleIndexPaths = self.tableView.indexPathsForVisibleRows
+        let midIndex = CGFloat(visibleIndexPaths!.count / 2)
+        UIView.setAnimationBeginsFromCurrentState(true)
+        for (index, indexPath) in (visibleIndexPaths?.enumerate())! {
+            let cell = self.tableView.cellForRowAtIndexPath(indexPath)
+            let rate = CGFloat(1 - abs(midIndex - CGFloat(index)) / midIndex)
+            cell?.contentView.alpha = rate
+        }
+        
     }
+    
+    override func scrollViewDidEndDecelerating(scrollView: UIScrollView) {
+        let distanceChanged = (scrollView.contentSize.height / 2 - scrollView.contentOffset.y - self.tableView.frame.size.height / 2) % (self.tableView.rowHeight * CGFloat(self.dataArray.count))
 
-
+        let movingOffset = CGPointMake(0, (self.tableView.contentSize.height - self.tableView.frame.size.height) / CGFloat(2.0) - distanceChanged)
+        print("Decelerating", distanceChanged, movingOffset, scrollView.contentOffset.y)
+        self.tableView.setContentOffset(movingOffset, animated: false)
+    }
 }
 
