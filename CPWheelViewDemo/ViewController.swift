b241f1147b4897755e8afc30f5b0e80c5bdf1cbf
ParkByounghyouk <chicpark7@gmail.com>
2016-03-08 16:45:18 +0900

    * [Bug fixed] After decelerating animation finished, contents in cell changed.

--- CPWheelViewDemo/ViewController.swift
+++ CPWheelViewDemo/ViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class ViewController: UITableViewController {
     var dataArray = [Int]()
-    var sampleInt = 0
+    var contentsOffset = 0
     override func viewDidLoad() {
         self.tableView.rowHeight = 60
         self.tableView.bounces = false
@@ -22,10 +22,11 @@ class ViewController: UITableViewController {
         for i in 0..<numberOfRows {
             self.dataArray.insert(i, atIndex: i)
         }
+
     }
     
     override func viewDidAppear(animated: Bool) {
-        self.tableView.selectRowAtIndexPath(NSIndexPath(forRow: dataArray.count / 2, inSection: 0), animated: false, scrollPosition: .Middle)
+        self.tableView.selectRowAtIndexPath(NSIndexPath(forRow: 5000, inSection: 0), animated: false, scrollPosition: .Middle)
     }
     
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -33,33 +34,44 @@ class ViewController: UITableViewController {
     }
     
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        if indexPath.row == 0 {
+        let realIndex = (indexPath.row + 1 + contentsOffset) % self.dataArray.count
+        if realIndex == 0 && indexPath.row != 0{
+            self.dataArray.insert(self.dataArray.first! - 1, atIndex: 0)
+            self.dataArray.removeLast()
+            contentsOffset++
         }
+
+        if realIndex == self.dataArray.count - 1 && indexPath.row != 0{
+            self.dataArray.append(self.dataArray.last! + 1)
+            self.dataArray.removeFirst()
+            contentsOffset--
+        }
+
         if let cell = tableView.dequeueReusableCellWithIdentifier("TEST") {
-            cell.textLabel?.text = "\(self.dataArray[indexPath.row % self.dataArray.count])"
+            cell.textLabel?.text = "\(self.dataArray[realIndex])"
             cell.selectionStyle = .Blue
             return cell
         }
+
         return UITableViewCell()
     }
 
     override func scrollViewDidScroll(scrollView: UIScrollView) {
-        let visibleIndexPaths = self.tableView.indexPathsForVisibleRows
-        let midIndex = CGFloat(visibleIndexPaths!.count / 2)
-        UIView.setAnimationBeginsFromCurrentState(true)
-        for (index, indexPath) in (visibleIndexPaths?.enumerate())! {
-            let cell = self.tableView.cellForRowAtIndexPath(indexPath)
-            let rate = CGFloat(1 - abs(midIndex - CGFloat(index)) / midIndex)
-            cell?.contentView.alpha = rate
+        if let visibleIndexPaths = self.tableView.indexPathsForVisibleRows {
+            let midIndex = CGFloat(visibleIndexPaths.count / 2)
+            UIView.setAnimationBeginsFromCurrentState(true)
+            for (index, indexPath) in (visibleIndexPaths.enumerate()) {
+                let cell = self.tableView.cellForRowAtIndexPath(indexPath)
+                let rate = CGFloat(1 - abs(midIndex - CGFloat(index)) / midIndex)
+                cell?.contentView.alpha = rate
+            }
         }
-        
     }
     
     override func scrollViewDidEndDecelerating(scrollView: UIScrollView) {
-        let distanceChanged = (scrollView.contentSize.height / 2 - scrollView.contentOffset.y - self.tableView.frame.size.height / 2) % (self.tableView.rowHeight * CGFloat(self.dataArray.count))
+        let distanceChanged = (scrollView.contentSize.height / 2 - scrollView.contentOffset.y - self.tableView.frame.size.height / 2) % (self.tableView.rowHeight * CGFloat(self.dataArray.count - self.contentsOffset))
 
         let movingOffset = CGPointMake(0, (self.tableView.contentSize.height - self.tableView.frame.size.height) / CGFloat(2.0) - distanceChanged)
-        print("Decelerating", distanceChanged, movingOffset, scrollView.contentOffset.y)
         self.tableView.setContentOffset(movingOffset, animated: false)
     }
 }
