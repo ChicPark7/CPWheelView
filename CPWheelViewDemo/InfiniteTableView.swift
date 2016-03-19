546250d9e74f1d2989e00503c7afaaeea74f3a7f
ChicPark7 <chicpark7@gmail.com>
2016-03-20 01:04:34 +0900

    * Add delegate to InfiniteTableView

--- CPWheelViewDemo/InfiniteTableView.swift
+++ CPWheelViewDemo/InfiniteTableView.swift
@@ -8,40 +8,52 @@
 
 import UIKit
 
-class InfiniteTableView: UIView, UITableViewDelegate, UITableViewDataSource {
-    var dataArray = [Int]()
-    var indexpathOffset = 0
-    let tableView = UITableView(frame: CGRectZero, style: .Plain)
-    var isAdjustingOffset = false
+public protocol InfiniteTableViewDelegate : NSObjectProtocol {
+    func identifierForNewElementAtTop(tableView: InfiniteTableView, topIdentifier: AnyObject) -> AnyObject
+    func identifierForNewElementAtBottom(tableView: InfiniteTableView, bottomIdentifier: AnyObject) -> AnyObject
+    func cellForNewElement(tableView: InfiniteTableView, identifier: AnyObject) -> UITableViewCell
+    func initialIdentifiers(tableView: InfiniteTableView, count: UInt) -> [AnyObject]
+}
+
+public class InfiniteTableView: UIView, UITableViewDelegate, UITableViewDataSource {
+    
+    public var delegate: InfiniteTableViewDelegate?
+    public var cellIdentifierArray = [AnyObject]()
+    public var cellHeight: CGFloat {
+        get  {
+            return tableView.rowHeight
+        }
+        set {
+            tableView.rowHeight = newValue
+            self.cellIdentifierArray = self.delegate!.initialIdentifiers(self, count: UInt(self.numberOfRows))
+        }
+    }
+    
+    private var indexpathOffset = 0
+    private let tableView = UITableView(frame: CGRectZero, style: .Plain)
+    private var isAdjustingOffset = false
+    private var numberOfRows: Int {
+        get {
+            let retVal = Int(frame.size.height / tableView.rowHeight) + 2
+            return retVal % 2 == 0 ? retVal + 1 : retVal
+        }
+    }
     
     override init(frame: CGRect) {
         super.init(frame: frame)
         initProperties()
     }
 
-    required init?(coder aDecoder: NSCoder) {
+    required public init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
         initProperties()
     }
     
-    override func layoutSubviews() {
-        super.layoutSubviews()
-        self.tableView.frame = self.bounds
-        self.dataArray.removeAll()
-        var numberOfRows = Int(self.frame.size.height / self.tableView.rowHeight) + 2
-        numberOfRows = numberOfRows % 2 == 0 ? numberOfRows + 1 : numberOfRows
-        for i in 0..<numberOfRows {
-            self.dataArray.insert(i, atIndex: i)
-        }
-        self.tableView.selectRowAtIndexPath(NSIndexPath(forRow: 50000 + self.dataArray.count / 2, inSection: 0), animated: false, scrollPosition: .Middle)
-        print(self.dataArray)
-    }
-    
     func initProperties() {
         self.addSubview(self.tableView)
-        self.tableView.delegate = self;
-        self.tableView.dataSource = self;
-        self.tableView.rowHeight = 200
+        self.tableView.rowHeight = 100
+        self.tableView.delegate = self
+        self.tableView.dataSource = self
         self.tableView.bounces = false
         self.tableView.separatorStyle = .None
         self.tableView.showsHorizontalScrollIndicator = false
@@ -49,43 +61,73 @@ class InfiniteTableView: UIView, UITableViewDelegate, UITableViewDataSource {
         self.tableView.registerClass(UITableViewCell.classForCoder(), forCellReuseIdentifier: "TEST")
     }
     
+    override public func layoutSubviews() {
+        super.layoutSubviews()
+        self.tableView.frame = self.bounds
+        self.cellIdentifierArray.removeAll()
+        for i in 0..<numberOfRows {
+            self.cellIdentifierArray.insert(i, atIndex: i)
+        }
+        self.tableView.selectRowAtIndexPath(NSIndexPath(forRow: 50000 + self.cellIdentifierArray.count / 2, inSection: 0), animated: false, scrollPosition: .Middle)
+    }
     
-    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    public func reloadData() {
+        self.tableView.reloadData()
+    }
+    
+    public func registerClass(cellClass: AnyClass?, forCellReuseIdentifier identifier: String) {
+        self.tableView.registerClass(cellClass, forCellReuseIdentifier: identifier)
+    }
+    
+    public func registerNib(nib: UINib?, forCellReuseIdentifier identifier: String) {
+        self.tableView.registerNib(nib, forCellReuseIdentifier: identifier)
+    }
+    
+    public func dequeueReusableCellWithIdentifier(identifier: String) -> UITableViewCell {
+        return tableView.dequeueReusableCellWithIdentifier(identifier)!
+    }
+    
+// MARK: Tableview delegate, datasource
+    
+    public func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 100000
     }
     
-    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let realIndex = (indexPath.row + indexpathOffset) % self.dataArray.count
-        let cell = tableView.dequeueReusableCellWithIdentifier("TEST")!
-        cell.textLabel?.text = "realIndex : \(realIndex)\nindex : \(indexPath.row)\ndata : \(self.dataArray[realIndex])"//
-        cell.textLabel?.numberOfLines = 3
-        cell.selectionStyle = .Blue
-
-        if !isAdjustingOffset && indexPath.row != 0{
-            if realIndex == 0 {
-                self.dataArray.insert(self.dataArray.first! - 1, atIndex: 0)
-                self.dataArray.removeLast()
-                indexpathOffset++
+    public func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let realIndex = (indexPath.row + indexpathOffset) % self.cellIdentifierArray.count
+        if let cell = delegate?.cellForNewElement(self, identifier: self.cellIdentifierArray[realIndex]), delegate = self.delegate {
+            cell.selectionStyle = .None
+            if !isAdjustingOffset && indexPath.row != 0{
+                if realIndex == 0 {
+                    let newIdentifier = delegate.identifierForNewElementAtTop(self, topIdentifier: self.cellIdentifierArray.first!)
+                    self.cellIdentifierArray.insert(newIdentifier, atIndex: 0)
+                    self.cellIdentifierArray.removeLast()
+                    indexpathOffset++
+                }
+                else if realIndex == self.cellIdentifierArray.count - 1{
+                    let newIdentifier = delegate.identifierForNewElementAtBottom(self, bottomIdentifier: self.cellIdentifierArray.last!)
+                    self.cellIdentifierArray.append(newIdentifier)
+                    self.cellIdentifierArray.removeFirst()
+                    indexpathOffset--
+                }
             }
-            
-            else if realIndex == self.dataArray.count - 1{
-                self.dataArray.append(self.dataArray.last! + 1)
-                self.dataArray.removeFirst()
-                indexpathOffset--
-            }
+                
+            return cell
         }
-        return cell
+        
+        return UITableViewCell();
     }
 
-    func scrollViewDidEndDecelerating(scrollView: UIScrollView) {
+    public func scrollViewDidEndDecelerating(scrollView: UIScrollView) {
+        print(self.cellIdentifierArray)
         isAdjustingOffset = true
-        let movedLevel = Int(self.tableView.contentOffset.y - self.tableView.contentSize.height / 2) / (dataArray.count * Int(self.tableView.rowHeight))
-        let newContentOffset = self.tableView.contentOffset.y - self.tableView.rowHeight * CGFloat(dataArray.count * movedLevel)
+        let movedLevel = Int(self.tableView.contentOffset.y - self.tableView.contentSize.height / 2) / (cellIdentifierArray.count * Int(self.tableView.rowHeight))
+        let newContentOffset = self.tableView.contentOffset.y - self.tableView.rowHeight * CGFloat(cellIdentifierArray.count * movedLevel)
         
         self.tableView.setContentOffset(CGPointMake(0, newContentOffset), animated: false)
         
         dispatch_async(dispatch_get_main_queue()) { () -> Void in
-            self.indexpathOffset = self.indexpathOffset + movedLevel * self.dataArray.count
+            self.indexpathOffset = self.indexpathOffset + movedLevel * self.cellIdentifierArray.count
             self.isAdjustingOffset = false
         }
     }
--- CPWheelViewDemo/ViewController.swift
+++ CPWheelViewDemo/ViewController.swift
@@ -8,6 +8,35 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, InfiniteTableViewDelegate {
+    
+    @IBOutlet var tableView: InfiniteTableView?
+    
+    override func viewDidLoad() {
+        tableView?.delegate = self
+        tableView?.cellHeight = 50
+    }
+    
+    func identifierForNewElementAtTop(tableView: InfiniteTableView, topIdentifier: AnyObject) -> AnyObject {
+        print(topIdentifier)
+        return topIdentifier as! Int - 1
+    }
+    
+    func identifierForNewElementAtBottom(tableView: InfiniteTableView, bottomIdentifier: AnyObject) -> AnyObject {
+        return bottomIdentifier as! Int + 1
+    }
+    
+    func cellForNewElement(tableView: InfiniteTableView, identifier: AnyObject) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("TEST")
+        cell.textLabel?.text = "\(identifier)"
+        return cell
+    }
+    
+    func initialIdentifiers(tableView: InfiniteTableView, count: UInt) -> [AnyObject] {
+        var retArray = [AnyObject]()
+        for i in 0...count {
+            retArray.append(i)
+        }
+        return retArray
+    }
 }
